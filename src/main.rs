4f962d0ffeefe5db33f222711f162d76afb70ddd
Mikael Lepistö <mikael.lepisto.ext@nokia.com>
2024-04-25 20:28:29 +0300

    Read json configuration and listen UDP address:port

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,63 @@
+use std::fs::File;
+use std::io::Read;
 use std::net::UdpSocket;
 
-fn main() -> std::io::Result<()> {
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct AddressConfig {
+    address: String,
+    port: u16,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct UniverseMappingConfig {
+    input: (u16,u16),
+    output: (u16,u16)
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct DeviceMappingConfig {
+    host: AddressConfig,
+    universes: Vec<UniverseMappingConfig>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Config {
+    listen: AddressConfig,
+    mappings: Vec<DeviceMappingConfig>,
+}
+
+fn read_config_file(file_path: &str) -> Result<Config,std::io::Error> {
+    let mut file = File::open(file_path)?;
+
+    // Read the file content into a string
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    // Deserialize the JSON string into a Config struct
+    let config: Config = serde_json::from_str(&contents)?;
+
+    // Now you can use the config object as needed
+    println!("{:?}", config);
+
+    Ok(config)
+}
+
+// TODO: create Device mapping output threads
+
+fn main() -> Result<(), Box<dyn std::error::Error>>  {
+
+    let config = match read_config_file("config.json") {
+        Ok(config) => config,
+        Err(err) => {
+            eprintln!("Error reading config file: {}", err);
+            return Err(Box::new(err));
+        }
+    };
+
     {
-        let socket = UdpSocket::bind("127.0.0.1:34254")?;
+        let socket = UdpSocket::bind(format!("{}:{}", config.listen.address, config.listen.port))?;
 
         // Receives a single datagram message on the socket. If `buf` is too small to hold
         // the message, it will be cut off.
@@ -14,5 +69,6 @@ fn main() -> std::io::Result<()> {
         buf.reverse();
         socket.send_to(buf, &src)?;
     } // the socket is closed here
+
     Ok(())
 }
